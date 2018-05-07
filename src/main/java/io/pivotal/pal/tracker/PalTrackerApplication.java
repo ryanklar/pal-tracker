004f48267546b7093e8e3844fe4e7d64c8cf95c3
ryanklar <ryan.klar@wellsfargo.com>
2018-05-07 09:37:24 -0600

    just test from ide

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    // test commit
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
